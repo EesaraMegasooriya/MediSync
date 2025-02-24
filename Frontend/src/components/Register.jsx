b5192d6c822073f08bd66fee697ec22e48e5c5b2
Your Name <you@example.com>
2025-02-24 16:09:54 +0530

    Reg

---
+++ Frontend/src/components/Register.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Register() {
+  return (
+    <div>
+      <div className="">
+        <div className="">Register</div>
+        
+      </div>
+
+    </div>
+  )
+}
+
+export default Register
